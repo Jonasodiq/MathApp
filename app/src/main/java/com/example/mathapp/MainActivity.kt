bef75ceed19df2f7d76a354b151d6f0375ffdf5b
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-11-10 18:18:38 +0100

    Added import

--- app/src/main/java/com/example/mathapp/MainActivity.kt
+++ app/src/main/java/com/example/mathapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.mathapp
 
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
