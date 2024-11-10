607f5605f7bb9bf9fbb4fd396a4de80d30892dc0
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-11-10 18:24:54 +0100

    Added AnswerActivity

--- app/src/main/java/com/example/mathapp/AnswerActivity.kt
+++ app/src/main/java/com/example/mathapp/AnswerActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mathapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AnswerActivity : AppCompatActivity() {
+
+    lateinit var resultView : TextView
+    lateinit var resultImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,28 @@ class AnswerActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        /*resultView kopplas till TextView-elementet resultView i layouten. En Button-variabel kopplas till knappen backButton.*/
+        resultView = findViewById(R.id.resultView)
+        resultImageView = findViewById(R.id.resultImageView)
+        val button = findViewById<Button>(R.id.backButton)
+
+        // När knappen trycks stängs AnswerActivity och återgår till föregående aktivitet genom att anropa finish().
+        button.setOnClickListener {
+            finish()
+        }
+
+        // Här hämtas värdet på answeredCorrect som skickades med Intent från MainActivity. Om inget värde skickades sätts standardvärdet till false.
+        val answer = intent.getBooleanExtra("answeredCorrect", false)
+
+        // Om användarens svar var rätt (answer är true), sätts texten i resultView till "Rätt svar". Annars sätts den till "Fel svar".
+        if (answer) {
+            resultView.text = "Rätt svar"
+            resultImageView.setImageResource(R.drawable.correct)
+        } else {
+            resultView.text = "Fel svar"
+            resultImageView.setImageResource(R.drawable.incorrect)
         }
     }
 }
